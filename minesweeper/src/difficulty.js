db690f82459bbbe87c657d291de976139bc9e128
Pavel Dalhou <70911716+mamont79@users.noreply.github.com>
2023-05-15 22:20:59 +0200

    feat: add buidHtml JS function

--- minesweeper/src/difficulty.js
+++ minesweeper/src/difficulty.js
@@ -4,21 +4,21 @@ const difficultLevel = [
     "width": 10,
     "height": 10,
     "class": "button-small active",
-
+    "level": 0,
   },
   {
     "title": "medium",
     "width": 15,
     "height": 15,
     "class": "button-medium",
-
+    "level": 1,
   },
   {
     "title": "hard",
     "width": 25,
     "height": 25,
     "class": "button-large",
-
+    "level": 2,
   },
 ];
 
--- minesweeper/src/init.js
+++ minesweeper/src/init.js
@@ -1,4 +1,4 @@
-import { difficultLevel } from "difficulty.js";
+import { difficultLevel } from "../src/difficulty.js";
 
 
 function buildHTML() {
@@ -10,24 +10,38 @@ function buildHTML() {
   let gameBox = '<div class="game-box"></div>';
   
   let controls = '<div class="controls"></div>';
-
   let difficult = '<div class="difficult"></div>';
-
   let mineField = '<div class="mine-field"></div>';
 
   let info = '<h2 class="info">Produced by Pavel Dalhou</h2>';
 
+  let controlItems = '<div class="new-game">New</div>';
+      controlItems += '<div class="aim">Aim:<input class="mines-number" value="10" name="mines-number" id="mines-number"></div>';
+      controlItems += '<div class="left">10 to Win</div>';
+      controlItems += '<div class="timer">0 sec</div>';
+
+  let buttons = '';
+  for (let i = 0; i < difficultLevel.length; i++) {
+    buttons += `<button class="${difficultLevel[i].class}" >${difficultLevel[i].title}</button>`;
+  }
 
   document.querySelector("body").innerHTML = centralizer;
   document.querySelector(".centralizer").innerHTML = title + gameBox + info;
   document.querySelector(".game-box").innerHTML = controls + difficult + mineField;
-
+  document.querySelector(".controls").innerHTML = controlItems;
+  document.querySelector(".difficult").innerHTML = buttons;
 }
 
+function buildField(level = 0) {
+  let insertText = '';
+  for (let i = 1; i <= difficultLevel[level].height; i++) {
+    insertText += '<div class="row">';
+    for (let j = 1; j <= difficultLevel[level].width; j++) {
+      insertText += `<div class="field" data="${i}-${j}"></div>`
+    }
+    insertText += '</div>';
+  }
+  document.querySelector(".mine-field").innerHTML = insertText;
+}
 
-
-// function buildField() {
-  
-// }
-
-export { buildHTML }
+export { buildHTML, buildField };
