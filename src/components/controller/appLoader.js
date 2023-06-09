f6f525752db135d3d27a023cb4f9ee6c94c3e682
Pavel Dalhou <70911716+mamont79@users.noreply.github.com>
2023-06-09 20:16:02 +0200

    feat: add files again

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader.js';
 class AppLoader extends Loader {
   constructor() {
     super('https://newsapi.org/v2/', {
-      apiKey: '', // получите свой ключ https://newsapi.org/
+      apiKey: '5608cc398fd54acca6a6384853eeaa4e',
     });
   }
 }
