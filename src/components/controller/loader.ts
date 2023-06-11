0a32575d35980b05a2b8369e5ee895b1bf57d0d6
Pavel Dalhou <dalhoupavel@gmail.com>
2023-06-11 12:13:26 +0200

    refactor: rename and fix loader and apploader

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,19 +1,27 @@
+import { IData } from '../app/appTypes.js';
+
+interface OptionLoader {
+  [key: string]: string;
+}
+
 class Loader {
-  constructor(baseLink, options) {
+  baseLink: string;
+  options: OptionLoader;
+  constructor(baseLink: string, options: OptionLoader) {
     this.baseLink = baseLink;
     this.options = options;
   }
 
   getResp(
-    { endpoint, options = {} },
+    { endpoint = '', options = {} },
     callback = () => {
       console.error('No callback for GET response');
-    },
+    }
   ) {
     this.load('GET', endpoint, callback, options);
   }
 
-  errorHandler(res) {
+  errorHandler(res: Response) {
     if (!res.ok) {
       if (res.status === 401 || res.status === 404)
         console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,7 +31,7 @@ class Loader {
     return res;
   }
 
-  makeUrl(options, endpoint) {
+  makeUrl(options: OptionLoader, endpoint: string) {
     const urlOptions = { ...this.options, ...options };
     let url = `${this.baseLink}${endpoint}?`;
 
@@ -34,7 +42,7 @@ class Loader {
     return url.slice(0, -1);
   }
 
-  load(method, endpoint, callback, options = {}) {
+  load(method: string, endpoint: string, callback: (data?: IData) => void, options = {}) {
     fetch(this.makeUrl(options, endpoint), { method })
       .then(this.errorHandler)
       .then((res) => res.json())
--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,18 +1,22 @@
 import News from './news/news.js';
 import Sources from './sources/sources.js';
+import { IData } from '../app/appTypes.js';
 
 export class AppView {
+  news: News;
+  sources: Sources;
+
   constructor() {
     this.news = new News();
     this.sources = new Sources();
   }
 
-  drawNews(data) {
+  drawNews(data?: IData) {
     const values = data?.articles ? data?.articles : [];
     this.news.draw(values);
   }
 
-  drawSources(data) {
+  drawSources(data?: IData) {
     const values = data?.sources ? data?.sources : [];
     this.sources.draw(values);
   }
