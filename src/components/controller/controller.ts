bf057725e420397de6070af45d8460f0f9bbbac6
Pavel Dalhou <dalhoupavel@gmail.com>
2023-06-18 18:18:19 +0200

    fix: fix issue with news

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -18,7 +18,7 @@ class AppController extends AppLoader {
     while (target !== newsContainer) {
       if (target.classList.contains('source__item')) {
         const sourceId = target.getAttribute('data-source-id') as string;
-        if (newsContainer.getAttribute('data-source') !== sourceId && !sourceId) {
+        if (newsContainer.getAttribute('data-source') !== sourceId && sourceId) {
           newsContainer.setAttribute('data-source', sourceId);
           super.getResp(
             {
