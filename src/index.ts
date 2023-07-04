6b8d136951e16616c0044cca7946a9c972152977
Pavel Dalhou <dalhoupavel@gmail.com>
2023-07-04 14:43:43 +0200

    feat: add showHelp function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './style.css';
 import * as levelData from './constants/levels.json';
 import * as baloonsCode from './constants/baloons.json';
+// import { help } from 'yargs';
 // import { ParamData, ITasksData } from './types/index';
 
 interface ICodeOfBaloon {
@@ -16,6 +17,11 @@ const codeArea = document.querySelector('.code-area') as HTMLElement;
 const baloonArea = document.querySelector('.baloons') as HTMLElement;
 
 const levelList = document.querySelector('.level-list') as HTMLElement;
+
+const checkMarks = document.querySelectorAll('.checkmark');
+const resetButton = document.querySelector('.reset') as HTMLElement;
+const helpButton = document.querySelector('.help') as HTMLElement;
+const helpText = document.querySelector('.help-text') as HTMLElement;
 
 let currentLevel: number;
 
@@ -45,14 +51,32 @@ const highlightLevel = (level: number) => {
 };
 highlightLevel(1);
 
+const hideHelp = () => {
+  helpText.classList.remove('shown');
+};
+
 const levelHandler = (level?: number) => {
   if (level) currentLevel = level;
   taskTitle.textContent = `${levelData[currentLevel].taskTitle}`;
   codeArea.innerHTML = levelData[currentLevel].shownCode || '';
   buildBaloon(currentLevel);
   highlightLevel(currentLevel + 1);
+  hideHelp();
   answerInput.value = '';
 };
+
+resetButton.addEventListener('click', () => {
+  checkMarks.forEach((element) => {
+    element.classList.remove('checked');
+  });
+  currentLevel = 0;
+  levelHandler(currentLevel);
+});
+
+helpButton.addEventListener('click', () => {
+  helpText.classList.add('shown');
+  helpText.textContent = levelData[currentLevel].help || null;
+});
 
 levelList.addEventListener('click', (event) => {
   if ((event.target as HTMLElement).classList.contains('level')) {
@@ -104,15 +128,4 @@ document.addEventListener('keyup', (event) => {
       showMistake();
     }
   }
-});
-
-const checkMarks = document.querySelectorAll('.checkmark');
-const resetButton = document.querySelector('.reset') as HTMLElement;
-
-resetButton.addEventListener('click', () => {
-  checkMarks.forEach((element) => {
-    element.classList.remove('checked');
-  });
-  currentLevel = 0;
-  levelHandler(currentLevel);
 });
