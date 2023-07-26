c664aba10310cab4d48f415ae549db7526ef71b9
Pavel Dalhou <dalhoupavel@gmail.com>
2023-07-26 16:22:47 +0200

    feat: add win result message

--- src/components/carAnimation/carAnimation.ts
+++ src/components/carAnimation/carAnimation.ts
@@ -1,3 +1,6 @@
+const winnersInfo: Array<string | null> = [];
+const winResultMessage = document.querySelector('.garage-win-result') as HTMLElement;
+
 const moveCurrentCar = (id: string) => {
   const currentLine = document.getElementById(`${id}-car`) as HTMLElement;
   const currentCar = document.getElementById(`${id}`) as HTMLElement;
@@ -19,8 +22,13 @@ const moveCurrentCar = (id: string) => {
       speed += carAcceleration;
       position += speed;
       if (position >= 100) {
+        (document.querySelector('.reset-button') as HTMLButtonElement).disabled = false;
         clearInterval(moveCar);
-        console.log(moves / 25);
+        winnersInfo.push(String(moves / 25));
+        winnersInfo.push((document.getElementById(`${id}-name`) as HTMLElement).textContent);
+        console.log(winnersInfo);
+        winResultMessage.textContent = `${winnersInfo[1]} win in ${winnersInfo[0]}`;
+        (document.getElementById(`${id}-reset`) as HTMLButtonElement).disabled = false;
       } else {
         moves += 1;
         currentCar.style.left = `${position}%`;
@@ -29,7 +37,10 @@ const moveCurrentCar = (id: string) => {
   } else {
     let moveCar = setInterval(() => {
       if (position >= loserDistanse) {
+        (document.querySelector('.reset-button') as HTMLButtonElement).disabled = false;
+
         clearInterval(moveCar);
+        (document.getElementById(`${id}-reset`) as HTMLButtonElement).disabled = false;
       } else if (position >= loserDistanse / 2) {
         position += speed;
         speed -= carAcceleration;
@@ -46,10 +57,16 @@ const moveCurrentCar = (id: string) => {
 const carToStart = (id: string) => {
   const currentCar = document.getElementById(`${id}`) as HTMLElement;
   currentCar.style.left = '0px';
+  winnersInfo.length = 0;
+  winResultMessage.classList.remove('active');
+  winResultMessage.textContent = `Race in progress...`;
   (document.getElementById(`${id}-start`) as HTMLButtonElement).disabled = false;
 };
 
 const allCarsToStart = () => {
+  (document.querySelector('.race-button') as HTMLButtonElement).disabled = false;
+  (document.querySelector('.reset-button') as HTMLButtonElement).disabled = false;
+
   const allCurrentCars = document.querySelectorAll('.car-image');
   allCurrentCars.forEach((element) => {
     const carId = element.id;
@@ -58,11 +75,14 @@ const allCarsToStart = () => {
 };
 
 const allCarMove = () => {
+  (document.querySelector('.race-button') as HTMLButtonElement).disabled = true;
+  (document.querySelector('.reset-button') as HTMLButtonElement).disabled = true;
   const allCurrentCars = document.querySelectorAll('.car-image');
   allCurrentCars.forEach((element) => {
     const carId = element.id;
     moveCurrentCar(carId);
   });
+  winResultMessage.classList.add('active');
 };
 
 export { moveCurrentCar, carToStart, allCarsToStart, allCarMove };
