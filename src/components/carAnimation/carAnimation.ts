c6a78b69a46df1e2e8f87a1ae5c4a20596847328
Pavel Dalhou <dalhoupavel@gmail.com>
2023-07-26 04:01:18 +0200

    feat: add chance of broken cars

--- src/components/carAnimation/carAnimation.ts
+++ src/components/carAnimation/carAnimation.ts
@@ -5,23 +5,42 @@ const moveCurrentCar = (id: string) => {
   const carWidth = 60;
 
   let carAcceleration = (Math.floor(Math.random() * 28) + 12) / 1000;
+  let loseChance = Math.random();
+  let loserDistanse = Math.random() * 70;
 
   let speed = 0;
   let position = 0;
   let moves = 0;
 
   (document.getElementById(`${id}-start`) as HTMLButtonElement).disabled = true;
-  let moveCar = setInterval(() => {
-    speed += carAcceleration;
-    position += speed;
-    if (position >= 100) {
-      clearInterval(moveCar);
-      console.log(moves / 25);
-    } else {
-      moves += 1;
-      currentCar.style.left = `${position}%`;
-    }
-  }, 40);
+
+  if (loseChance < 0.8) {
+    let moveCar = setInterval(() => {
+      speed += carAcceleration;
+      position += speed;
+      if (position >= 100) {
+        clearInterval(moveCar);
+        console.log(moves / 25);
+      } else {
+        moves += 1;
+        currentCar.style.left = `${position}%`;
+      }
+    }, 40);
+  } else {
+    let moveCar = setInterval(() => {
+      if (position >= loserDistanse) {
+        clearInterval(moveCar);
+      } else if (position >= loserDistanse / 2) {
+        position += speed;
+        speed -= carAcceleration;
+        currentCar.style.left = `${position}%`;
+      } else {
+        speed += carAcceleration;
+        position += speed;
+        currentCar.style.left = `${position}%`;
+      }
+    }, 40);
+  }
 };
 
 const carToStart = (id: string) => {
